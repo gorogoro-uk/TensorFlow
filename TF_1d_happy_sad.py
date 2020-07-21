84316fecc9b105e0637559c2147a0c2b8a9a2060
gorogoro-uk <67004639+gorogoro-uk@users.noreply.github.com>
2020-07-21 18:52:53 +0100

    add tensor dimension

--- TF_1d_happy_sad.py
+++ TF_1d_happy_sad.py
@@ -1,41 +1,46 @@
 # Happy-Sad image classification
 
 import tensorflow as tf
-import wget
+from pathlib import Path
+# import wget
 import urllib.request
 import os
-#from pathlib import path
 import zipfile
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import RMSprop
 
 # data import
-# wget does not work with python (but does in Google colab)
 #!wget --no-check-certificate \
 #    "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip" \
 #    -O "/hs/happy-or-sad.zip"
 
-# create directory in root
-#os.mkdir(os.path.join(os.getcwd(), 'hs'))
-#os.mkdir(os.path.join(os.getcwd(), 'hs/happy_sad'))
+# define locations
+BASE = Path(os.getcwd()) / 'happysad'
+HS_DATA = BASE / 'hs_data'
+ZIP_DEST = BASE / 'happy-or-sad.zip'
+ZIP_URL = "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip"
+print(f"BASE:     {BASE}")
+print(f"HS_DATA:  {HS_DATA}")
+print(f"ZIP_DEST: {ZIP_DEST}")
+print(f"ZIP_URL:  {ZIP_URL}")
 
-zip_dest = os.path.join(os.getcwd(),'/hs/happy-or-sad.zip')
-unzip_dest = os.path.join(os.getcwd(),'/hs/happy_sad/')
-print(zip_dest, unzip_dest)
+# make directories
+if not os.path.exists(BASE):
+    os.mkdir(BASE)
+if not os.path.exists(HS_DATA):
+    os.mkdir(HS_DATA)
 
-
-zip_url = "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip"
-urllib.request.urlretrieve(zip_url, zip_dest)
-
-zip_ref = zipfile.ZipFile(zip_dest, 'r')
-zip_ref.extractall(unzip_dest)
+# download data file & unzip
+urllib.request.urlretrieve(ZIP_URL, ZIP_DEST)
+zip_ref = zipfile.ZipFile(ZIP_DEST, 'r')
+zip_ref.extractall(HS_DATA)
 zip_ref.close()
 
 # data generator
 image_data_gen = ImageDataGenerator(rescale=1/255.0)
 
 train_data_gen = image_data_gen.flow_from_directory(
-    "/hs/happy_sad",
+    HS_DATA,
     target_size=(150, 150),
     batch_size=10,
     class_mode='binary'
@@ -52,18 +57,19 @@ class accCallBack(tf.keras.callbacks.Callback):
 happysad_acc_callback = accCallBack()
 
 # model definition
+# input: [batch=10, height=150, width=150, rgb=3]
 happysad_model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),    # output: [10, 148, 148, 16]
+    tf.keras.layers.MaxPooling2D(2,2),                                                  # output: [10, 74, 74, 16]
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),                              # output: [10, 72, 72, 32]
+    tf.keras.layers.MaxPooling2D(2, 2),                                                 # output: [10, 36, 36, 32]
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),                              # output: [10, 34, 34, 32]
+    tf.keras.layers.MaxPooling2D(2, 2),                                                 # output: [10, 17, 17, 32]
+    tf.keras.layers.Flatten(),                                                          # output: [10, 9248]
+    tf.keras.layers.Dense(512, activation='relu'),                                      # output: [10, 512]
+    tf.keras.layers.Dense(1, activation='sigmoid')                                      # output: [10, 1]
 ])
-happysad_model.model_summary()
+happysad_model.summary()
 
 # compile model
 happysad_model.compile(
